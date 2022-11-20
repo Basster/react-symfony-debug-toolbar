26f16283967688027843c747f0f62349fa08ad36
Ole Rößner <o.roessner@neusta.de>
2022-11-20 18:31:12 +0100

    run tests with vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
@@ -19,5 +20,10 @@ export default defineConfig({
         },
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
   },
 });
